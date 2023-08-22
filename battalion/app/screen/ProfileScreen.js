8eb5739c6b5211fa92f88bfc2bac44f09acdbb86
sup25 <suparnaa51@gmail.com>
2023-08-22 21:06:24 +0545

    updated profile screen

--- battalion/app/screen/ProfileScreen.js
+++ battalion/app/screen/ProfileScreen.js
@@ -1,21 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   Text,
   View,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
 } from "react-native";
 import { useAuth } from "../utils/AuthProvider";
-
+import * as LocalAuthentication from "expo-local-authentication";
 import colors from "../config/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { updateProfile } from "firebase/auth";
 
 const ProfileScreen = ({ navigation }) => {
   const { currentUser } = useAuth();
+
   const userEmail = currentUser?.email;
-  const userName = currentUser?.displayName;
+
   const phoneNumber = currentUser?.phoneNumber;
+  const [isChangingEmail, setIsChangingEmail] = useState(false);
+  const [isEditingEmail, setIsEditingEmail] = useState(false);
+  const [isEditingUsername, setIsEditingUsername] = useState(false);
+
+  const [updatedUserName, setUpdatedUserName] = useState(
+    currentUser?.displayName || ""
+  );
+  const handleEmailChange = async () => {
+    if (!isEditingEmail) {
+      try {
+        const hasBiometricHardware =
+          await LocalAuthentication.hasHardwareAsync();
+
+        if (hasBiometricHardware) {
+          const isBiometricEnrolled =
+            await LocalAuthentication.isEnrolledAsync();
+
+          if (isBiometricEnrolled) {
+            const biometricResult = await LocalAuthentication.authenticateAsync(
+              {
+                promptMessage: "Authenticate to change email",
+              }
+            );
+
+            if (biometricResult.success) {
+              // User successfully authenticated using biometrics
+              setIsEditingEmail(true); // Enable email editing
+              setIsChangingEmail(true); // Also enable onFocus effect
+            } else {
+              // Biometric authentication failed
+              console.log("Biometric authentication failed");
+            }
+          } else {
+            // Biometric not enrolled
+            console.log("Biometric not enrolled");
+          }
+        } else {
+          // Biometric hardware not available
+          console.log("Biometric hardware not available");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  const handleUsernameEdit = () => {
+    setIsEditingUsername(!isEditingUsername);
+  };
+
+  const handleSaveUserName = async () => {
+    try {
+      if (currentUser) {
+        const newUserName = updatedUserName.trim();
+
+        if (newUserName === "") {
+          console.log("Username cannot be empty");
+          return;
+        }
+        console.log(currentUser.displayName);
+
+        await updateProfile(currentUser, {
+          displayName: newUserName,
+        });
+
+        console.log("Profile update successful");
+        setIsEditingUsername(false);
+      } else {
+        console.log("User is not available");
+      }
+    } catch (error) {
+      console.log("Error updating username:", error);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -24,34 +100,60 @@ const ProfileScreen = ({ navigation }) => {
         <View style={styles.bigRectangle}>
           <View>
             <Text style={styles.userName}>UserName</Text>
-            <TextInput
-              style={styles.inputs}
-              placeholder=""
-              placeholderTextColor="white"
-              value={userName}
-            />
-            <MaterialCommunityIcons
-              name="pencil"
-              color="#5A5A5A"
-              size={30}
-              style={styles.icon}
-            />
+            {isEditingUsername ? (
+              <TextInput
+                style={styles.inputs}
+                value={updatedUserName}
+                onChangeText={setUpdatedUserName}
+              />
+            ) : (
+              <Text style={styles.inputs}>{updatedUserName}</Text>
+            )}
+            {isEditingUsername ? (
+              <MaterialCommunityIcons
+                name="check"
+                color="#5A5A5A"
+                size={30}
+                style={styles.icon}
+                onPress={handleSaveUserName}
+              />
+            ) : (
+              <MaterialCommunityIcons
+                name="pencil"
+                color="#5A5A5A"
+                size={30}
+                style={styles.icon}
+                onPress={handleUsernameEdit}
+              />
+            )}
           </View>
-          <View>
-            <Text style={styles.email}>Email</Text>
-            <TextInput
-              style={styles.inputs}
-              placeholder=""
-              placeholderTextColor="white"
-              value={userEmail}
-            />
-            <MaterialCommunityIcons
-              name="pencil"
-              color="#5A5A5A"
-              size={30}
-              style={styles.icon}
-            />
-          </View>
+
+          <TouchableWithoutFeedback onPress={handleEmailChange}>
+            <View>
+              <Text style={styles.email}>Email</Text>
+              <TextInput
+                style={[
+                  styles.inputs,
+                  {
+                    backgroundColor: isEditingEmail ? "#1B1B1B" : "transparent",
+                  },
+                ]}
+                placeholder=""
+                placeholderTextColor="white"
+                value={userEmail}
+                editable={isEditingEmail}
+                onFocus={() => setIsChangingEmail(true)}
+                onBlur={() => setIsChangingEmail(false)}
+              />
+              <MaterialCommunityIcons
+                name="pencil"
+                color="#5A5A5A"
+                size={30}
+                style={styles.icon}
+                onPress={handleEmailChange}
+              />
+            </View>
+          </TouchableWithoutFeedback>
           <View>
             <Text style={styles.phoneNumber}>Phone Number</Text>
             <TextInput
@@ -187,6 +289,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     padding: 10,
     color: colors.white,
+    justifyContent: "center",
   },
   icon: {
     position: "absolute",
